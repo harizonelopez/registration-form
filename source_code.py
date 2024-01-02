61efaa9fd1442815ffaef2a272c2b95f656c30da
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-01-02 14:13:24 +0300

    Update to the registration form

--- source_code.py
+++ source_code.py
@@ -1,34 +1,37 @@
 from tkinter import *
+import tkinter as tk
 
 root=Tk()
 
 root.title("@Dev_aladinh ")
 root.geometry( )
+
 root.resizable(True,True)
 
-def Register():
-    print("You are succesfully registerd to SIT 100 unit ^'^ ")
+def register_user():
+    print("Congrats!!, you are succesfully registerd to SIT101 unit curriculum.")
 
-Label(root,text="SIT 100 Unit Registration Form", font="arial 40").pack(pady=50)
+Label(root,text="SIT101 Course Unit Registration Form.", background = "indigo", font="arial 30").place(x=400, y=60)
 
 Label(text="Name", font=30).place(x=100,y=150)
 Label(text="Registration Number", font=30).place(x=100,y=250)
 Label(text="Email", font=30).place(x=100,y=350)
-Label(text="Gender", font=30).place(x=100,y=450)
-Label(text="School of Study", font=30).place(x=100,y=550)
+Label(text="Gender", font=30).place(x=100,y=550)
+Label(text="School of Study", font=30).place(x=100,y=450)
 
-#entry
+#entry data
 nameValue=StringVar()
 registrationnumberValue=StringVar()
 emailValue=StringVar()
 genderValue=StringVar()
 schoolofstudyValue=StringVar()
 
+#entry fields
 nameEntry=Entry(root,textvariable=nameValue, width=40, bd=2, font=20)
 registrationEntry=Entry(root,textvariable=registrationnumberValue, width=40, bd=2, font=20)
 emailEntry=Entry(root,textvariable=emailValue, width=40, bd=2, font=20)
 schoolofstudyEntry=Entry(root,textvariable=schoolofstudyValue, width=40, bd=2, font=20)
-#genderEntry=Entry(root,textvariable=genderValue, width=40, bd=2, font=20)
+
 #check gender
 checkValue=IntVar
 checkbtn=Checkbutton(text="male", font=10, variable=checkValue)
@@ -39,7 +42,6 @@ checkbtn.place(x=500, y=550)
 nameEntry.place(x=400, y=150)
 registrationEntry.place(x=400, y=250)
 emailEntry.place(x=400, y=350)
-#genderEntry.place(x=400, y=450)
 schoolofstudyEntry.place(x=400, y=450)
 
 #check button 
@@ -47,6 +49,8 @@ checkValue=IntVar
 Checkbtn=Checkbutton(text="Remember me?", font=10, variable=checkValue)
 Checkbtn.place(x=600, y=600)
 
-Button(text="Register",font=10, background="blue").place(x=600, y=650)
+#to add a command to it
+register_button = tk.Button(root, text="Register", font=10, background="blue", command = register_user)
+register_button.place(x=600, y=650)
                
 root.mainloop()
